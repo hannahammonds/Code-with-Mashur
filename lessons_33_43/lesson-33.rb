ef45898c14ccba1d86e085b7a86ed3e1e71b78d6
hannahammonds <90658994+hannahammonds@users.noreply.github.com>
2022-02-24 06:27:15 -0600

    Initial commit

---
+++ lessons_33_43/lesson-33.rb
@@ -0,0 +1,9 @@
+#class name are generally CamelCase
+#class definitions look like this -> class SomeClass
+    #definitions start with capital
+#snake_case for file names
+
+#google "ruby style guide" for more reference 
+
+#tabs/indents use 2 spaces
+
---
+++ lessons_33_43/lesson-40.rb
@@ -0,0 +1,8 @@
+#object oriented programming - uses objects and their interactions to design and program applications
+
+#see university_project
+
+#to make a setter you have to have an = in definition to use when setting during calling method
+
+#attirbute accessors - will give both getter and setters for specificed attributes
+#attirubes are symbols or identies for that class
